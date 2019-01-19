1d0516e5346e93701ad09e0421dcb4fc9ff0ff51
Carsten Jaksch <jaksch.carsten@gmail.com>
2019-01-19 21:35:49 +0100

    Changed metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const packages = require(`./src/packages.json`)
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `npm.cardiv.de`,
+    description: `Personal NPM favorites library.`,
+    author: `@cardiv`,
     packages: packages,
   },
   plugins: [
